941f19bb41e152d7f2183031a081f62e2c0aab03
Diksha Gupta <gdiksha1312@gmail.com>
2024-07-10 03:43:46 +0200

    stage 3 done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.Stage_01_data_ingestion import DataIngestionPipeline
 from textSummarizer.pipeline.Stage_02_data_validation import DataValidationPipeline
+from textSummarizer.pipeline.Stage_03_data_transformation import DataTransformationPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -18,6 +19,17 @@ STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f"---------------- STAGE {STAGE_NAME} STARTED ----------------")
     data_ingestion = DataValidationPipeline()
+    data_ingestion.main()
+    logger.info(f"---------------- STAGE {STAGE_NAME} COMPLETED --------------- \n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f"---------------- STAGE {STAGE_NAME} STARTED ----------------")
+    data_ingestion = DataTransformationPipeline()
     data_ingestion.main()
     logger.info(f"---------------- STAGE {STAGE_NAME} COMPLETED --------------- \n\n")
 except Exception as e:
