af90c23e6b2b6d8feeb08873e8aa577f93168376
Diksha Gupta <gdiksha1312@gmail.com>
2024-07-10 03:20:00 +0200

    stage 1 completed

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from textSummarizer.logging import logging
+from textSummarizer.logging import logger
+from textSummarizer.pipeline.Stage_01_data_ingestion import DataIngestionPipeline
 
-logging.info("Welcome")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f"---------------- STAGE {STAGE_NAME} STARTED ----------------")
+    data_ingestion = DataIngestionPipeline()
+    data_ingestion.main()
+    logger.info(f"---------------- STAGE {STAGE_NAME} COMPLETED --------------- \n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Ingestion Stage"
